ada93753ee242f63776924a532209a674d20aaf3
Александр Бучельников <a.buchelnikov99@gmail.com>
2023-11-12 10:59:24 +0100

    add methot set things

--- game_classes.py
+++ game_classes.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
 
 @dataclass
@@ -13,9 +13,16 @@ class Person:
     default_attack: int
     default_deffent: int
 
-    def set_things(things):
+    def set_things(self, things):
         """Принимает список вещей."""
+        for thing in things:
+            self._count_buff(**asdict(thing))
 
-    def attack_damage():
+    def _count_buff(self):
+        """Обновление свойств персонажа"""
+        
+
+
+    def attack_damage(self):
         """Вычитает жизни на основе атаки."""
         pass
