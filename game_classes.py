aa0a5269e021f4995733f296dfe3faf2e068dbb5
Александр Бучельников <a.buchelnikov99@gmail.com>
2023-11-12 10:22:56 +0100

    Add class Person with methods

--- game_classes.py
+++ game_classes.py
@@ -12,3 +12,10 @@ class Person:
     health: int
     default_attack: int
     default_deffent: int
+
+    def set_things(things):
+        """Принимает список вещей."""
+
+    def attack_damage():
+        """Вычитает жизни на основе атаки."""
+        pass
