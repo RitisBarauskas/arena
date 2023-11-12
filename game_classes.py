bde36b12e19c05fea0bb45686d036c8f3b2c3b3f
EPONCHINTSEV DMITRII <deponchintsevv@gmail.com>
2023-11-12 12:42:25 +0400

    write create Thing class; create other classes structure

--- game_classes.py
+++ game_classes.py
@@ -2,9 +2,30 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Persan:
+class Thing:
+    """Создает объект инвентаря."""
+    title: str
+    defense_percentage: int
+    attack: int
+    health_point: int
+
+
+@dataclass
+class Person:
     """
     Создает персонажа.
 
     """
     health: int
+
+
+@dataclass
+class Paladin(Person):
+    """Создает персонажа типа: Plaldin"""
+    ...
+
+@dataclass
+class Warrior(Person):
+    """Создает персонажа типа: Warrior"""
+    ...
+
